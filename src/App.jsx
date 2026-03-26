e9f4117c1e33ea052eee71f17a01abdebbf2bf67
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-03-26 19:27:49 +0600

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen'>
+        <h1 className='text-5xl font-bold'>Dragon News</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
