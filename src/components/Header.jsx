5ce56cfcb2e400624f633e550b3ab3df4dac33ff
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-03-27 15:38:23 +0600

    customized theme and displayed current date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import logo from '../assets/logo.png'
+import React from "react";
+import logo from "../assets/logo.png";
 
 const Header = () => {
-    return (
-        <div className='flex justify-center flex-col items-center gap-3'>
-            <img src={logo} alt="Logo" />
-            <p>Journalism Without Fear or Favour</p>
-        </div>
-    );
+  const date = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  return (
+    <div className="flex justify-center flex-col items-center gap-3">
+      <img src={logo} alt="Logo" />
+      <p className="text-accent">Journalism Without Fear or Favour</p>
+      <p className="font-semibold text-accent">{date}</p>
+    </div>
+  );
 };
 
 export default Header;
