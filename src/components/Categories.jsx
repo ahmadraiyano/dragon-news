7a49d0688491a57a14608b9f1613f5b1ec510b4b
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-03-28 19:37:08 +0600

    displayed category wise dynamic route and created social login button

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,15 @@
 import React, { use } from 'react';
+import { NavLink } from 'react-router';
 
 const categoryPromise = fetch("/categories.json").then(res=> res.json())
 const Categories = () => {
     const categories = use(categoryPromise)
     return (
         <div>
-            All categories ({categories.length})
+            <h2 className='font-bold'>All categories ({categories.length})</h2>
+            <div className='grid grid-cols-1 mt-4 gap-2'>
+                {categories.map(category=> <NavLink className='btn bg-base-100 border-0 hover:bg-base-200 font-semibold text-accent' key={category.id} to={`/category/${category.id}`}>{category.name}</NavLink>)}
+            </div>
         </div>
     );
 };
