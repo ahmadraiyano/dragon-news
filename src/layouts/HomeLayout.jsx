a4b4e59ccf3e7ecd7b468ab173cd17b7c6efde69
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-03-29 13:30:41 +0600

    implemented sticky scroll

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,37 +1,36 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import Header from '../components/Header';
-import LatestNews from '../components/LatestNews';
-import Navbar from '../components/Navbar';
-import LeftAside from '../components/HomeLayout/LeftAside';
-import RightAside from '../components/homeLayout/RightAside';
-
+import React from "react";
+import { Outlet } from "react-router";
+import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
+import LeftAside from "../components/HomeLayout/LeftAside";
+import RightAside from "../components/homeLayout/RightAside";
 
 const HomeLayout = () => {
-    return (
-        <div>
-            <header>
-                <Header></Header>
-                <section className='w-11/12 lg:w-10/12 mx-auto my-3'>
-                    <LatestNews></LatestNews>
-                </section>
-                <nav className='w-11/12 lg:w-10/12 mx-auto my-3'>
-                    <Navbar></Navbar>
-                </nav>
-            </header>
-            <main className='w-11/12 lg:w-10/12 mx-auto my-3 grid grid-cols-1 lg:grid-cols-4'>
-                <aside>
-                    <LeftAside></LeftAside>
-                </aside>
-                <section className="main col-span-2 lg:mx-4">
-                    <Outlet></Outlet>
-                </section>
-                <aside>
-                    <RightAside></RightAside>
-                </aside>
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <header>
+        <Header></Header>
+        <section className="w-11/12 lg:w-10/12 mx-auto my-3">
+          <LatestNews></LatestNews>
+        </section>
+        <nav className="w-11/12 lg:w-10/12 mx-auto my-3">
+          <Navbar></Navbar>
+        </nav>
+      </header>
+      <main className="w-11/12 lg:w-10/12 mx-auto my-3 grid grid-cols-1 lg:grid-cols-4">
+        <aside className="lg:sticky lg:top-0 lg:h-screen lg:overflow-y-auto">
+          <LeftAside></LeftAside>
+        </aside>
+        <section className="main col-span-2 lg:mx-4">
+          <Outlet></Outlet>
+        </section>
+        <aside className="lg:sticky lg:top-0 lg:h-screen lg:overflow-y-auto">
+          <RightAside></RightAside>
+        </aside>
+      </main>
+    </div>
+  );
 };
 
 export default HomeLayout;
