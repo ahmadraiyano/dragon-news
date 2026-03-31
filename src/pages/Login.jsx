35cb94fe43dba0b610f4df96306f5c1aa5c76958
Ahmad Raiyan <ahmadraiyanofficial@gmail.com>
2026-03-31 23:57:13 +0600

    deployed to firebase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,8 +16,8 @@ const Login = () => {
 
     createLogin(email, password)
       .then((result) => {
-        const user = result.user;
-        console.log(user);
+        const _user = result.user;
+        // console.log(user);
         navigate(location.state? location.state: '/')
       })
       .catch((error) => {
